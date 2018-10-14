3e0fa8a526b5299f09c5f993b6094b9040d79694
Duc Tho Tran <ducthotran2010@gmail.com>
2018-10-14 21:02:03 +0700

    Add word for kanji

--- Kanji/fetch.js
+++ Kanji/fetch.js
@@ -1,4 +1,4 @@
-Words.push(new Word('会', 'Hội'));
+﻿Words.push(new Word('会', 'Hội'));
 Words.push(new Word('言', 'Ngôn'));
 Words.push(new Word('安', 'An'));
 Words.push(new Word('万', 'Vạn'));
@@ -9,3 +9,27 @@ Words.push(new Word('主', 'Chủ, Chúa'));
 Words.push(new Word('乗', 'Thừa - lên xe'));
 Words.push(new Word('事', 'Sự'));
 
+
+Words.push(new Word('世', 'Thế'));
+
+Words.push(new Word('京', 'Kinh - kinh đô'));
+
+Words.push(new Word('使', 'Sử - sử dụng'));
+Words.push(new Word('作', 'Tác - làm/tạo nên/tạo tác'));
+Words.push(new Word('体', 'Thể - thân thể/cơ thể'));
+Words.push(new Word('住', 'Trụ/Trú'));
+Words.push(new Word('低', 'Đê - thấp/không cao'));
+Words.push(new Word('以', 'Dĩ - bởi vì/lí do'));
+Words.push(new Word('代', 'Đại - triều đại/đại diện'));
+Words.push(new Word('仕', 'Sĩ'));
+
+Words.push(new Word('便', 'Tiện'));
+Words.push(new Word('借', 'Tá - vay mượn'));
+Words.push(new Word('働', 'Động - lao động/vận động'));
+Words.push(new Word('元', 'Nguyên - bắt đầu/thứ nhất/nguyên tố'));
+Words.push(new Word('兄', 'Huynh'));
+Words.push(new Word('光', 'Quang'));
+Words.push(new Word('写', 'Tả - diễn tả/đặc tả'));
+Words.push(new Word('冬', 'Đông'));
+Words.push(new Word('切', 'Thiết - cắt/chạm khắc'));
+Words.push(new Word('別', 'Biệt - biệt li/đặc biệt/riêng biệt'));

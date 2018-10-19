a4030a262a974928e414ad05770820936f5cc8ac
Duc Tho Tran <ducthotran2010@gmail.com>
2018-10-19 13:09:42 +0700

    Add kanji v4

--- Kanji/fetch.js
+++ Kanji/fetch.js
@@ -1,11 +1,19 @@
 ﻿Words.push(new Word('会', 'Hội'));
+
 Words.push(new Word('言', 'Ngôn'));
+
 Words.push(new Word('安', 'An'));
+
 Words.push(new Word('万', 'Vạn'));
+
 Words.push(new Word('間', 'Gian'));
+
 Words.push(new Word('空', 'Không'));
+
 Words.push(new Word('不', 'Bất, Phi'));
+
 Words.push(new Word('主', 'Chủ, Chúa'));
+
 Words.push(new Word('乗', 'Thừa - lên xe'));
 Words.push(new Word('事', 'Sự'));
 
@@ -25,7 +33,7 @@ Words.push(new Word('仕', 'Sĩ'));
 
 Words.push(new Word('便', 'Tiện'));
 Words.push(new Word('借', 'Tá - vay mượn'));
-Words.push(new Word('働', 'Động - lao động/vận động'));
+Words.push(new Word('働', 'Động - lao động/vận động~ nói về làm việc'));
 Words.push(new Word('元', 'Nguyên - bắt đầu/thứ nhất/nguyên tố'));
 Words.push(new Word('兄', 'Huynh'));
 Words.push(new Word('光', 'Quang'));
@@ -33,3 +41,14 @@ Words.push(new Word('写', 'Tả - diễn tả/đặc tả'));
 Words.push(new Word('冬', 'Đông'));
 Words.push(new Word('切', 'Thiết - cắt/chạm khắc'));
 Words.push(new Word('別', 'Biệt - biệt li/đặc biệt/riêng biệt'));
+
+Words.push(new Word('賃', 'Nhẫm - thuê mượn'));
+Words.push(new Word('弟', 'Đệ'));
+Words.push(new Word('姉', 'Tỷ'));
+Words.push(new Word('妹', 'Muội'));
+Words.push(new Word('力', 'Lực'));
+Words.push(new Word('勉', 'Miễn - Gắng sức, cố sức'));
+Words.push(new Word('動', 'Động - động trong động đậy/cảm động/tự động'));
+Words.push(new Word('区', 'Khu - khu vực/vùng'));
+Words.push(new Word('医', 'Y'));
+Words.push(new Word('去', 'Khứ - đi/bỏ/đã qua'));
